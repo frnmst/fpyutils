c815f5ee565dae5e7fc512df6a6a387ea83463f7
Franco Masotti <franco.masotti@live.com>
2020-12-19 15:31:58 +0100

    Preparing for new release.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fpyutils',
-    version='1.2.1',
+    version='1.2.2',
     packages=find_packages(exclude=['*tests*']),
     license='GPL',
     description='A collection of useful non-standard Python functions which aim to be simple to use, highly readable but not efficient.',
