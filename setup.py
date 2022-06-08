580d34ab95b17f31ed7d7e3ccc094146ba329ef7
Franco Masotti <franco.masotti@tutanota.com>
2022-06-08 17:28:26 +0200

    Minor fixes.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #
 # setup.py
 #
-# Copyright (C) 2017-2019 Franco Masotti (franco \D\o\T masotti {-A-T-} tutanota \D\o\T com)
+# Copyright (C) 2017-2022 Franco Masotti (franco \D\o\T masotti {-A-T-} tutanota \D\o\T com)
 #
 # This file is part of fpyutils.
 #
